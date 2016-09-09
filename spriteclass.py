ecc42f4ba04668a7cbcae2a353f06ed78404c88d
Braidfifteen <raatkin15@gmail.com>
2016-09-08 20:01:08 -0400

    Working as intended. Some methods not in final destination.

--- spriteclass.py
+++ spriteclass.py
@@ -1,16 +1,27 @@
 import pygame as pg
 import prepare as p
 
-
+DIRECT_DICT = {"UP": (0, -1),
+               "RIGHT": (1, 0),
+               "DOWN": (0, 1),
+               "LEFT": (-1, 0)}
+               
+CONTROLS = {pg.K_w: "UP", 
+            pg.K_d: "RIGHT",
+            pg.K_s: "DOWN",
+            pg.K_a: "LEFT"
+           }
+            
+            
 class Sprite(pg.sprite.DirtySprite):
     """Basic sprite class for all actors."""
-    def __init__(self, pos, size, color *groups):
+    def __init__(self, pos, size, color, *groups):
         super().__init__(*groups)
         self.image = pg.Surface(size)
         self.image.fill(color)
         self.rect = self.image.get_rect(center=pos)
         self.velocity = [0, 0]
-        
+        self.speed = 5
 
 class Player(Sprite):
     """Class that represents user controlled sprite."""
@@ -23,12 +34,36 @@ class Player(Sprite):
         
     def get_event(self, event):
         """Gets events from game and passes them to Player."""
-        pass
+        if event.type == pg.KEYDOWN:
+            if event.key in CONTROLS:
+                direction = CONTROLS[event.key]
+                self.velocity = DIRECT_DICT[direction]
+        elif event.type == pg.KEYUP:
+            self.velocity = [0, 0]
+            
         
-    def update(self):
+    def update(self, room):
         """Update image and position of sprite."""
-        pass
+        old_pos = self.rect.topleft
+        move = self.velocity[0] * self.speed, self.velocity[1] * self.speed
+        self.rect.move_ip(move)
+        self.wall_hit_logic(move, room.walls)
+        if self.rect.topleft != old_pos:
+            self.dirty = 1
         
+    def wall_hit_logic(self, move, walls):
+        wall_hit_list = pg.sprite.spritecollide(self, walls, False)
+        for wall in wall_hit_list:
+            if move[0] > 0:
+                self.rect.right = wall.rect.left
+            elif move[0] < 0:
+                self.rect.left = wall.rect.right
+            elif move[1] > 0:
+                self.rect.bottom = wall.rect.top
+            elif move[1] < 0:
+                self.rect.top = wall.rect.bottom
+            break
+                
         
 class Enemies(Sprite):
     """Basic enemy class."""
