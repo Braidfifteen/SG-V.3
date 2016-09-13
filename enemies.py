5f2c3e570bbf587283ee34cac2e9706c7e82bcca
Braidfifteen <raatkin15@gmail.com>
2016-09-12 20:39:56 -0400

    Health bar working. Work in progress.

--- enemies.py
+++ enemies.py
@@ -1,18 +1,40 @@
 import pygame as pg
 import prepare as p
+import constants as c
+from gui import HealthBar
 
             
 
         
 class Enemies(pg.sprite.DirtySprite):
     """Basic enemy class."""
-    def __init__(self, player, pos, size, color, *groups):
-        super().__init__(pos, size, color, *groups)
+    def __init__(self, game, player, pos, size, *groups):
+        super().__init__(*groups)
+        self.image = pg.Surface(size).convert()
+        self.image.fill(c.RED)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = pos
         self.player = player
-        self.attributes = None
+        self.game = game
+        self.health = 100
+        self.damage = 10
+        self.dirty = 1
+        self.health_bar = HealthBar(self, (self.health, 15), (self.rect.center[0]-10,
+                          self.rect.center[1]-10), self.game.all_sprites)
+        self.dead = False
         
-    def update(self):
-        pass
+    def update(self, room):
+        self.health_bar.update(self.health, self.dead)       
+        if self.dead == False:
+
+            bullet_hit = pg.sprite.spritecollide(self, room.bullet_container, True)
+            for bullet in bullet_hit:
+                self.health -= bullet.gun.damage
+                bullet.kill()
+            if self.health <= 0:
+                self.kill()
+                self.dead = True
+        
         
         
     
--- guns.py
+++ guns.py
@@ -33,6 +33,9 @@ class Bullet(pg.sprite.DirtySprite):
             self.rect.y > c.SCREEN_SIZE[1]:
             self.kill()
             
+    def calculate_bullet_range(self):
+        pass
+        
             
 class Gun():
     def __init__(self, player):
@@ -40,13 +43,12 @@ class Gun():
         self.damage = 10
         self.fire_rate = 400
         self.ammo_capacity = 50
-        self.range = 90
+        self.range = 700
         self.is_shooting = False
         self.fire_rate_timer = 0
         
     def update(self, dt):
         self.fire_rate_timer += dt
-        print(self.fire_rate_timer)
         if self.is_shooting and self.fire_rate_timer >= self.fire_rate:
             pos = pg.mouse.get_pos()
             mouse_x = pos[0]
